0542c524a2ff8ef443345a998d5f0407ab42ffc5
karina-253 <k.pinoeva@g.nsu.ru>
2026-02-04 08:22:06 +0700

    Update search.py

--- search.py
+++ search.py
@@ -378,4 +378,4 @@ def format_windows_search_results(results: List[Dict[str, Any]],
 
     print("=" * 80)
     print(f"{lcl.ALL_FIND} {len(results)} {lcl.FILES_2}\n")
-                                    
+                                      
